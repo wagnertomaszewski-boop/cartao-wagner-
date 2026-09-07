6a192763c85d3d0f94dd4330c14c428f0425be88
wagnertomaszewski-boop <wagner.tomaszewski@gmail.com>
2026-09-07 13:20:17 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "wagner-app-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./solicitacao.html",
+  "./acompanhar.html",
+  "./cartao-wagner.jpg",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
